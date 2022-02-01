9c836ac585061cdcd1c124d9c49be313143e2206
Fernando Baeza Hurtado <91624660+ferbaeza@users.noreply.github.com>
2022-02-01 10:55:46 +0100

    carrou

--- src/components/headfoot/Carrousel.js
+++ src/components/headfoot/Carrousel.js
@@ -1,7 +1,43 @@
 
-        </div>
-        </>
-    )
-}
-export default Carrousel;
 
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+
+
+export default class NextJsCarousel extends Component {
+	render() {
+		return (
+			<div className="text-center col-12 caja">
+
+			<Carousel>
+				<div>
+					<img src="/images/1.png" alt="image1" className='imagen'/>
+					{/* <p className="legend">Image 1</p> */}
+
+				</div>
+				<div>
+					<img src="/images/2.png" alt="image2" className='imagen'/>
+					{/* <p className="legend">Image 2</p> */}
+
+				</div>
+				<div>
+					<img src="/images/3.png" alt="image3" className='imagen'/>
+					{/* <p className="legend">Image 3</p> */}
+
+				</div>
+				<div>
+					<img src="/images/4.png" alt="image4" className='imagen'/>
+					{/* <p className="legend">Image 4</p> */}
+
+				</div>
+                
+
+			</Carousel>
+           
+           
+			</div>
+		);
+	}
+};
