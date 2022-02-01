32abf35a54fddf6b7a075a488f9556118e7abe01
Fernando Baeza Hurtado <91624660+ferbaeza@users.noreply.github.com>
2022-02-02 00:55:52 +0100

    id

--- src/pages/restaurants/[id].js
+++ src/pages/restaurants/[id].js
@@ -6,36 +6,27 @@ export default function Reviews({ newinfo }) {
   return (
       <>
         <div className="text-center">
-            <h1 className="text-center fs-1 text-secondary">Videos</h1>
+            <h1 className="text-center fs-1 text-secondary">Reviews</h1>
         </div>
         <div className='container'>
             <div class="row row-cols-1">
                 <div class="card text-dark bg-light mb-3">
-                    <div class="card-header bg-primary text-white text-center" key={newinfo.id}>{newinfo.pubDate}</div>
-                        <div class="card-body">
-                            <h5>{newinfo.title}</h5>
-                        </div>
-                        <p class="card-text">{newinfo.description}</p>
-
-                            <a href={`https://www.youtube.com/watch?v=${newinfo.guid}`} class="btn stretched-link">
-                            <button type="button" class="btn btn-danger rounded-circle">
-                                <Image src="/images/tube.svg" width={80} height={80} alt="Youtube"/>
-                            </button>
-                            </a>
-                            <span className="text-center">Ver en Youtube</span>
-
-
-
-
-
-                        <div class="card-header bg-primary text-white text-center">{newinfo.guid}</div>
+                    <div class="card-header bg-info text-white text-center" key={newinfo.id}></div>
+                        <p class="card-text fs-4">{newinfo.description}</p>
+                        <p>
+                        <span className="bg-secondary fs-5">Escrito por: </span><p class="card-text">{newinfo.email}</p>
+                        </p>
+                        <p>
+                        <span className="bg-secondary fs-5">Puntuacion: </span><p class="card-text">{newinfo.puntuation}</p>
+                        </p>
+                        <div class="card-header bg-info text-white text-center">{newinfo.guid}</div>
                     </div>
                     <div className="back text-center">
                         <div className="p-1 mb-7 ml-7 bg-dark text-white" id="head">
                             <ul className="nav justify-content-center fs-3">
                                 <li className="nav-item">
-                                <Link href="/videos/videos">
-                                    <a className="nav-link text-muted" aria-current="page">Videos</a>
+                                <Link href="/restaurants/restaurants">
+                                    <a className="nav-link text-muted" aria-current="page">Go back</a>
                                 </Link>
                                 </li>
                             </ul>
@@ -52,7 +43,7 @@ export default function Reviews({ newinfo }) {
 }
 
 export async function getServerSideProps({ params }) {
-  const res = await fetch(`http://appmurcia_codeigniter.test/rest/videos/${params.id}`);
+  const res = await fetch(`http://appmurcia_codeigniter.test/rest/reviewrestauranteid/${params.id}`);
   const newinfo = await res.json();
   return {
     props: {
