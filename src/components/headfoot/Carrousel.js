530a1517048d0bb3278379f721acf1d200ea821b
Fernando Baeza Hurtado <91624660+ferbaeza@users.noreply.github.com>
2022-02-01 10:53:26 +0100

    refresh

--- src/components/headfoot/Carrousel.js
+++ src/components/headfoot/Carrousel.js
@@ -1,15 +1,4 @@
-// import Image from "next/image"
-// const Carrousel=()=>{
-//     return(
-//         <>
-//         <div className="container m-auto ">
-//         <Image src="/images/m2.jpg" class="m-auto" width={1200} height={400} alt="..."/>
 
-//         </div>
-//         </>
-//     )
-// }
-// export default Carrousel;
 
 import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
