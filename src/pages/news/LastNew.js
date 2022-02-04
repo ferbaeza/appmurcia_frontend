25a1597d52d9f93a38926c1cdfe3a994ac2a4837
Fernando Baeza Hurtado <91624660+ferbaeza@users.noreply.github.com>
2022-02-04 18:01:13 +0100

    last

--- src/pages/news/LastNew.js
+++ src/pages/news/LastNew.js
@@ -1,21 +1,30 @@
-export async function getStaticProps() {
-    const res = await fetch("http://appmurcia_codeigniter.test/rest/news_one")
-    const data = await res.json()
-    console.log(data.id)
-    return {
-      props: {
-        data: data
-    }
-  } 
-}
+import { useEffect, useState } from "react";
 
-  export default function LastNew({data}){
-    console.log(data)
-    return(
-        <>
-            <div className="text-center">
-                <h1 className="text-center fs-1 text-secondary">Ultima Noticia</h1>
-            </div>
-        </>
-    )  
+// export async function getStaticProps() {
+//   const res = await fetch("http://appmurcia_codeigniter.test/rest/news_one/")
+//   const last = await res.json()
+//   return {
+//     props: {
+//       last
+//   }
+//   } 
+// }
+
+export default function LastNew({last}){
+  let [title, setTitle] = useState(null)
+    useEffect(async () => {
+    await fetch("http://appmurcia_codeigniter.test/rest/news_one")
+      .then((response) => response.json())
+      .then((data) => setTitle(data.title))      
+      .then((data) => console.log(data));
+  });
+
+  return(
+      <>
+          <div className="text-center">
+              <h1 className="text-center fs-1 text-secondary">Ultima Hora</h1>
+              <h4>{title}</h4>
+          </div>
+      </>
+  )  
 }
